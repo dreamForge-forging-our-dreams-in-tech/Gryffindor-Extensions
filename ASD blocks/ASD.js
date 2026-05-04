c733a9f59d2338e3e5f03a55976bb8bd6c92153b
Luke-technolab <luke@technolableiden.nl>
2026-05-04 09:48:04 +0200

    added ASD blocks to the [lugin repo

---
+++ ASD blocks/ASD.js
@@ -0,0 +1,57 @@
+// Adds an ASD block to the turbowarp editor
+// It takes a string of code entered in CODE and evaluates it as Javascript
+// there are two types COMMAND and REPORTER
+// COMMAND executes a piece of code and returns nothing, while REPORTER executes a piece of code and returns a value.
+
+class ASD_COMMAND {
+    getInfo() {
+        return {
+            id: 'gryffindorjstools', // Ensure this is lowercase/no symbols
+            name: 'JS Tools',
+            color1: '#0088ff', // Main block color
+            color2: '#0066cc', // Hover/Outline color
+            blocks: [
+                {
+                    opcode: 'evalCode',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Evaluate [CODE]',
+                    arguments: {
+                        CODE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'a * 2'
+                        },
+
+                    }
+                },
+                {
+                    opcode: 'evalCode_Reporter',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Evaluate [CODE] and return result',
+                    arguments: {
+                        CODE: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'a * 2'
+                        },
+
+                    }
+                }
+            ]
+        };
+    }
+
+    // The function name MUST match the opcode above exactly
+    evalCode_Reporter(args) {
+        return this.evalCode(args); // reuse evalCode to avoid code duplication, only reason for extra function is due to how turbowarp handles identification of blocks.
+    }
+
+    evalCode(args) {
+        try {
+            const fn = new Function('a', `return ${args.CODE}`);
+            return fn(); // keep return statement because reporter reuses the same function
+        } catch (e) {
+            return `Error: ${e.message}`;
+        }
+    }
+}
+
+Scratch.extensions.register(new ASD_COMMAND());
---
+++ ASD blocks/ASD_readme.md
@@ -0,0 +1,13 @@
+# ASD (Add source Directly) blocks
+
+# IMPORTANT
+This extension  can execute harmfull code, if you do not have to run this unsandboxed then DO NOT run it unsandboxed!
+Only run it unsandboxed if you know what you are doing and absolutely need to use it!
+
+## REPORTER
+The REPORTER block of the ASD block family evaluates the given piece of code and rturns the result.
+![alt text](<scratchblocks (2).png>)
+
+## COMMAND
+The COMMAND block of the ASD block family evaluates the given piece of code without returning a result.
+![alt text](scratchblocks.png)
