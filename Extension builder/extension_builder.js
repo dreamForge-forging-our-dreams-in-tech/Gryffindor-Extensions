a8abac275dfd297ac050b0bdf59f24501f54341b
Luke-technolab <luke@technolableiden.nl>
2026-06-04 13:40:27 +0200

    testing a fix for a issue where sinleline code could not be rendered because substacks would then be forced onto a singleline aswell, breaking the code

--- Extension builder/extension_builder.js
+++ Extension builder/extension_builder.js
@@ -173,6 +173,12 @@ async function buildBlocks(file_name, Scratch) {
                         if (i.substring(0, 1) === '[' && i.substring(i.length - 1) === ']') { // This is a placeholder for a value from the block (e.g. [VALUE] or [ARGUMENTS])
                             if (i == '[TRANSPILE]') {
                                 let substackCode = this.transpile(substackId, target); // Recursively transpile the substack blocks to get the code for the substack and insert it into the code block. This allows users to write blocks that include substacks and have the code for those substacks be included in the generated code correctly.
+
+                                if (code_json[opcodeWithoutExtension].singleLine) {
+                                    codeToInsert += '\n'; //make sure transpiled substacks are actually on a new line even if newline is set to true
+                                    substackCode += '\n';
+                                }
+
                                 codeToInsert += substackCode; // Insert the transpiled substack code into the lines array so it will be included in the generated code.
                                 continue; // Skip the rest of the loop for this iteration since we've already handled this placeholder
                             }
