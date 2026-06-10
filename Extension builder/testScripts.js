60c50803b3e829acb3fc722aaf9ba6f131d88425
Luke-technolab <luke@technolableiden.nl>
2026-06-10 15:45:28 +0200

    made ASD blocks compatible with the extension again, this is a temporary workaround as i didnt feel like create another whole new codebase just to generate raw code.

--- Extension builder/testScripts.js
+++ Extension builder/testScripts.js
@@ -1,31 +1,29 @@
+class MyCoolAndAwesomeExtension {
+  constructor(runtime = {}) {
+    this.runtime = runtime;
 
-
-            class MyCoolAndAwesomeExtension {
-            constructor(runtime = {}) {
-            this.runtime = runtime;
-            
-            }
-
-            getInfo() {
-            return {
-            id: 'test',
-name: 'test',
-            blocks: [
-{
-opcode: 'opcode_name',
-blockType: Scratch.BlockType.COMMAND,
-text: 'test',
-},
-
-],
- 
-            }
-            }
-              
-            
- async opcode_name (args, util) {
-try {new Function('a', `return 'alert()'`.replaceAll(`'`, ``))();} catch (e) {console.log(`Error: ${e.message}`);}
   }
 
-            }
-            Scratch.extensions.register(new MyCoolAndAwesomeExtension());
+  getInfo() {
+    return {
+      id: 'tes',
+      name: 'tes',
+      blocks: [
+        {
+          opcode: 'opcode_name',
+          blockType: Scratch.BlockType.COMMAND,
+          blockType: 'test',
+        },
+
+      ],
+
+    }
+  }
+
+
+  async opcode_name(args, util) {
+    try { new Function('a', `return 'alert()'`.replaceAll(`'`, ``))(); } catch (e) { console.error(`Error: ${e.message}`); }
+  }
+
+}
+Scratch.extensions.register(new MyCoolAndAwesomeExtension());
