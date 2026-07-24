1192e865de0b43002531bd3dcaca80301a0031d1
Luke-technolab <luke@technolableiden.nl>
2026-07-24 15:32:16 +0200

    changed the way the extension id is extruded from the blocks, now it repalces it with a empty string instead of sub stringing it.
    added variable support, turbowarp variables used inside of your cvode are now automatically turned into js variables, expect show and hide as they dont work if a varialbe will never be shown anyways, still WIP

--- Extension builder/extension_builder.js
+++ Extension builder/extension_builder.js
@@ -144,9 +144,11 @@ window.registerExtension = async (url) => {
             while (currentId) {
                 const block = target.blocks.getBlock(currentId);
                 const opcode = block.opcode;
-                const opcodeWithoutExtension = opcode.replaceAll(window.extension_id + '_', ''); // Remove the extension_id prefix from the opcode to get the base opcode name, which is used in the directory.json file to find the corresponding code block for this opcode. This allows us to reuse the same code blocks for multiple extensions without having to duplicate them in the directory.json file.
+                const opcodeWithoutExtension = opcode.replaceAll(extension_id + '_', ''); // Remove the extension_id prefix from the opcode to get the base opcode name, which is used in the directory.json file to find the corresponding code block for this opcode. This allows us to reuse the same code blocks for multiple extensions without having to duplicate them in the directory.json file.
                 let code_json;
                 let i;
+
+                console.log(opcodeWithoutExtension);
 
                 for (i in window.code_directory) { //find the correct file path conencted to the opcode in the directory.json file so we can use it to get the code block for that opcode. This allows us to easily organize our code blocks into different files and keep the transpile function clean and organized.
 
